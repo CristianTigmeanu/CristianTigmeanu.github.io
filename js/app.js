fd6ec7929215b64ce5d57bba1eee840744053920
CristianTigmeanu <cristi.claudiu99@gmail.com>
2022-02-19 15:57:46 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ funcCreateCards = (newMovieData) =>{
     <p>${nMD.year}</p>
     <p>IMDB Rating: <span>${nMD.imDbRating}</span></p>
     <div class="movieCardSocial">
-    <a href="#"><i class="fas fa-heart"></i></a>
+    <a href="#" onClick="funcLike('${nMD.id}')"><i class="fas fa-heart ${nMD.likes ? 'likeHeart': '' }"></i></a>
     <a href="https://www.imdb.com/title/${nMD.id}/" target="_blank" ><i class="fas fa-share-alt"></i></a>
     <a href="#"><i class="fas fa-comment"></i></a>
     </div>
@@ -59,3 +59,28 @@ funcCreateCards = (newMovieData) =>{
   document.querySelector('body').innerHTML = tempCardData;
 };
 //===============================================================
+
+
+
+
+//Likes clicked
+funcLike = (i) => {
+//console.log(i);
+
+  movieData = movieData.map((m)=>{
+if (m.id === i) {
+  m.likes+=1;
+}
+return m;
+});
+//console.log(movieData);
+
+movieData.sort(function(a, b) {
+  return b.likes - a.likes;
+});
+
+
+funcCreateCards(movieData);
+};
+
+
